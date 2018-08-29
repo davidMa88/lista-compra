f5c9bc62cedc85d8f8388fc596cdef31ecaa98ce
davidMa88 <davidmae20@gmail.com>
2018-08-30 01:01:16 +0200

    Node.js !

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,9 +7,25 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      sidebar: false
+      sidebar: false,
+      response: ""
     };
   }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch("/api/hello");
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
 
   enableSidebar(sidebarToggle) {
     this.setState({ sidebar: sidebarToggle });
@@ -21,6 +37,7 @@ class App extends Component {
         <HeaderApp sidebar={this.enableSidebar.bind(this)} />
         <BodyApp sidebarToggle={this.state.sidebar} />
         <FooterApp />
+        <p className="App-intro">{this.state.response}</p>
       </React.Fragment>
     );
   }
